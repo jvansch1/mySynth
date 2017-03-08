5ab7959acf97ed83f360a3af04a035784655e6c6
John Van Schultz <jvansch1@binghamton.edu>
2017-03-08 16:53:09 -0500

    Refactor all tuna effects and events into inidivudal files

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,22 @@ module.exports = {
     entry: "./lib/main.js",
     output: {
         path: __dirname,
-        filename: "./lib/my_synth.js"
+        filename: "./lib/my_synth.js",
     },
     module: {
-      loaders: [
-        {
-          loader: 'babel-loader'
+    loaders: [
+      {
+        test: [/\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
         }
-      ]
-    }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '*']
+  },
 };
